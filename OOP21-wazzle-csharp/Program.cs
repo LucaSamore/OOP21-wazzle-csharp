efe8e9477795a5b873af812bdb891a6ef934e80f
Luca Samore <luca.samore@studio.unibo.it>
2022-05-03 18:23:29 +0200

    Add project files.

---
+++ OOP21-wazzle-csharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OOP21_wazzle_csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
